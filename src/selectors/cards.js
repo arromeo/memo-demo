8886934396eb6e848c720789db1d78d965eed93b
Adam Romeo <adam.r.romeo@gmail.com>
2020-04-10 21:37:54 -0700

    Add empty array reference to selectors

--- src/selectors/cards.js
+++ src/selectors/cards.js
@@ -4,8 +4,10 @@ export const allCards = (state) => Object.values(state.cards)
 
 export const cardById = (id) => (state) => state.cards[id]
 
+const emptyArray = []
+
 export const childCardNames = (id) => (state) => {
-    console.log('happening')
-  if (state.cards[id].children.length === 0) return []
+  console.log('--------------------------')
+  if (state.cards[id].children.length === 0) return emptyArray
   return state.cards[id].children.map((child) => state.cards[child].value)
 }
