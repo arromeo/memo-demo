a5adbe3b30cd10e3f73ed3c1a6223db35084576b
Adam Romeo <adam.r.romeo@gmail.com>
2020-04-10 22:22:31 -0700

    Add some mock data

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -1,6 +1,30 @@
 import { cardActions } from '../actions/cards'
 
-export const cardsReducer = (state = {}, action) => {
+// Start with some mock data
+const initialState = {
+  1: {
+    id: '1',
+    value: 'Card 1',
+    children: []
+  },
+  2: {
+    id: '2',
+    value: 'Card 2',
+    children: ['1']
+  },
+  3: {
+    id: '3',
+    value: 'Card 3',
+    children: ['2']
+  },
+  4: {
+    id: '4',
+    value: 'Card 4',
+    children: ['2', '3']
+  }
+}
+
+export const cardsReducer = (state = initialState, action) => {
   switch (action.type) {
     case cardActions.ADD_CARD:
       return {
